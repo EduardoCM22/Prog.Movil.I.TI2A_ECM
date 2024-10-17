cbda8bfb087c10c3d9d72bc7cd3a9aba9a5d79cd
edwcm <none>
2024-10-16 22:58:07 -0600

    Cómo implementar el diseño de contenido adaptable

--- CompilaAppDisenoAdaptable/basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/ui/ReplyHomeContent.kt
+++ CompilaAppDisenoAdaptable/basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/ui/ReplyHomeContent.kt
@@ -49,6 +49,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import com.example.reply.R
 import com.example.reply.data.Email
 import com.example.reply.data.local.LocalAccountsDataProvider
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
 fun ReplyListOnlyContent(
@@ -116,10 +117,8 @@ fun ReplyListAndDetailContent(
         val activity = LocalContext.current as Activity
         ReplyDetailsScreen(
             replyUiState = replyUiState,
-            modifier = Modifier
-                .padding(top = dimensionResource(R.dimen.email_list_item_vertical_spacing))
-                .weight(1f),
-            onBackPressed = {}
+            modifier = Modifier.weight(1f),
+            onBackPressed = { activity.finish() }
         )
     }
 }
