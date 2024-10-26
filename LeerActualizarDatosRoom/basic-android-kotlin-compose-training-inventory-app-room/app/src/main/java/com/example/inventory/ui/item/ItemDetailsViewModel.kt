0351015f68deaf749153eec967637af2780c594d
edwcm <none>
2024-10-25 23:04:49 -0600

    Implementa un elemento Sell (App Completa)

--- LeerActualizarDatosRoom/basic-android-kotlin-compose-training-inventory-app-room/app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
+++ LeerActualizarDatosRoom/basic-android-kotlin-compose-training-inventory-app-room/app/src/main/java/com/example/inventory/ui/item/ItemDetailsViewModel.kt
@@ -53,6 +53,19 @@ class ItemDetailsViewModel(
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    fun reduceQuantityByOne() {
+        viewModelScope.launch {
+            val currentItem = uiState.value.itemDetails.toItem()
+            if (currentItem.quantity > 0) {
+                itemsRepository.updateItem(currentItem.copy(quantity = currentItem.quantity - 1))
+            }
+        }
+    }
+
+    suspend fun deleteItem() {
+        itemsRepository.deleteItem(uiState.value.itemDetails.toItem())
+    }
 }
 
 /**
